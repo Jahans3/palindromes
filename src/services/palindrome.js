0ecd55104468e941c312ecf24038fd55875bf94d
Admin <josh.jahans3@gmail.com>
2017-09-29 22:20:17 +0100

    isPalindrome function

--- src/services/palindrome.js
+++ src/services/palindrome.js
@@ -1,1 +1,21 @@
-module.exports = {}
+const isPalindrome = ({ palindrome = '' }) => {
+  if (!palindrome.length) return false
+
+  const removeSpecials = palindrome.replace(/[^A-Za-z]/g, '')
+  const lowerCase = removeSpecials.toLowerCase()
+  const chars = lowerCase.split('')
+  const reversed = [...chars].reverse() // don't mutate chars
+  let result = true
+
+  reversed.map((char, i) => {
+    if (char !== chars[i]) {
+      result = false
+    }
+  })
+
+  return result
+}
+
+module.exports = {
+  isPalindrome
+}
